f872c08e65f45a4f7bba977ddd8695de47f32f7f
Pradip <Pradip@DESKTOP-7D2H6S8>
2025-07-01 13:07:23 +0530

    Adding the create token logic in Asp.Net Core Part 10

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -1,12 +1,45 @@
-﻿using API.Interfaces;
+﻿using API.Entities;
+using API.Interfaces;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace API.Services
 {
     public class TokenService : ITokenService
     {
-        public string CreateToken()
+
+        private readonly SymmetricSecurityKey _key;
+
+        public TokenService(IConfiguration config)
         {
-            throw new NotImplementedException();
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+        }
+
+
+        public string CreateToken(AppUser user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+            };
+
+            var creds = new SigningCredentials(_key, SecurityAlgorithms.EcdsaSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
         }
     }
 }
