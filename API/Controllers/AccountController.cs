e909e1f154eb2dcf3bb299d34c0aaf32e8bd5928
Pradip <Pradip@DESKTOP-7D2H6S8>
2025-07-01 12:24:59 +0530

    Adding a login endpoint in Asp.Net Core Part 7

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -42,6 +42,32 @@ namespace API.Controllers
         }
 
 
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto) 
+        {
+            var user = await db.Users.SingleOrDefaultAsync(m => m.UserName == loginDto.UserName);
+
+            if (user == null)
+            {
+                return Unauthorized("Invalid username");
+            }
+
+            var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswardHash[i])
+                {
+                    return Unauthorized("Invalid password");
+                }
+            }
+
+            return user;
+        }
+        
+
         private async Task<bool> UserExists(string username)
         {
             return await db.Users.AnyAsync(m => m.UserName == username.ToLower());
