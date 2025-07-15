bc8292a2fec3ccbf329af2859e2655fbe25e59dc
Pradip <Pradip@DESKTOP-7D2H6S8>
2025-07-15 15:19:27 +0530

    Using the angular bootstrap component dropdown in Asp.Net Core part 19

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -4,8 +4,7 @@ import { AccountService } from '../_services/account.service';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styles: [
-  ]
+  styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
 
