703b148f55cc248a31bd37254ab23f8818fad923
Pradip <Pradip@DESKTOP-7D2H6S8>
2025-08-08 16:11:42 +0530

    Repository Pattern in Asp.Net Core part 45

---
+++ API/Data/UserRepository.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DataContext db;
+        public UserRepository(DataContext context)
+        {
+            this.db = context;
+        }
+
+        public async Task<AppUser> GetUserByIdAsync(int id)
+        {
+            return await db.Users.FindAsync(id);
+        }
+
+        public async Task<AppUser> GetUserByUsernameAsync(string username)
+        {
+            return await db.Users.SingleOrDefaultAsync(x => x.UserName == username);
+        }
+
+        public async Task<IEnumerable<AppUser>> GetUsersAsync()
+        {
+            return await db.Users.ToListAsync();
+        }
+
+        public async Task<bool> SaveAllAsync()
+        {
+            return await db.SaveChangesAsync() > 0;
+        }
+
+        public void Update(AppUser user)
+        {
+            db.Entry(user).State = EntityState.Modified;
+        }
+    }
+}
