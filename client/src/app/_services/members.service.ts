214aed6860aa066e9890acf51f2d2be7f60e671a
Pradip <Pradip@DESKTOP-7D2H6S8>
2025-08-29 15:22:53 +0530

    Retrieving List & Creating Member Card in Asp.Net Core part 51

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -3,9 +3,10 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 
+
 const httpOptions = {
   headers: new HttpHeaders({
-    Authorization: 'Bearer' + JSON.parse(localStorage.getItem('user')).token
+    Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user'))?.token
   })
 }
 
