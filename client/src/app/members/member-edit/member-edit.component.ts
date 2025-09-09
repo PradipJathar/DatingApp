c0c7143a0355d110cd3960d179aaff5450a85b09
Pradip <Pradip@DESKTOP-7D2H6S8>
2025-09-09 13:37:20 +0530

    Persisting the changes in the API in Asp.Net Core part 60

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -40,9 +40,10 @@ export class MemberEditComponent implements OnInit {
   }
 
   updateMember() {
-    console.log(this.member);
-    this.toastr.success('Profile updated successfully.');
-    this.editForm.reset(this.member);
+    this.membersService.updateMember(this.member).subscribe(() => {
+      this.toastr.success('Profile updated successfully.');
+      this.editForm.reset(this.member);
+    })
   }
 
 }
