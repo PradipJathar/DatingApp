4c669286af9fec4bcfdd7fd4065fcbde651af4f5
Pradip <Pradip@DESKTOP-7D2H6S8>
2025-10-09 10:19:18 +0530

    Adding helper classes for pagination in Asp.Net Core part 83

---
+++ API/Helpers/UserParams.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Helpers
+{
+    public class UserParams
+    {
+        private const int _maxPageSize = 50;
+        public int PageNumber { get; set; }
+        private int _pageSize = 10;
+
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = (value > _maxPageSize) ? _maxPageSize : value;
+        }
+
+    }
+}
