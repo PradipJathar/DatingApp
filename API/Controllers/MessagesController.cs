360243ca34f5ad292b277266830899fb0317add3
Pradip <Pradip@DESKTOP-7D2H6S8>
2025-10-13 00:15:46 +0530

    Getting the message thread for 2 users in Asp.Net Core part 112

--- API/Controllers/MessagesController.cs
+++ API/Controllers/MessagesController.cs
@@ -76,5 +76,14 @@ namespace API.Controllers
 
             return messages;
         }
+
+
+        [HttpGet("thread/{username}")]
+        public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessageThread(string username)
+        {
+            var currentUsername = User.GetUsername();
+
+            return Ok(await _messageRepo.GetMessageThread(currentUsername, username));
+        }
     }
 }
