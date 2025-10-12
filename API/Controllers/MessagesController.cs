a11fc4d28b5dedb7eb6792d9b506f361f18a2191
Pradip <Pradip@DESKTOP-7D2H6S8>
2025-10-12 22:50:38 +0530

    Adding a message controller in Asp.Net Core part 110

---
+++ API/Controllers/MessagesController.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
+using API.Helpers;
+using API.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class MessagesController : BaseApiController
+    {
+        private readonly IUserRepository _userRepo;
+        private readonly IMessageRepository _messageRepo;
+        private readonly IMapper _mapper;
+
+        public MessagesController(IUserRepository userRepo, IMessageRepository messageRepo,
+            IMapper mapper)
+        {
+            _userRepo = userRepo;
+            _messageRepo = messageRepo;
+            _mapper = mapper;
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<MessageDto>> CreateMessage(CreateMessageDto createMessageDto)
+        {
+            var username = User.GetUsername();
+
+            if (username == createMessageDto.RecipientUsername.ToLower())
+            {
+                return BadRequest("You cannot send message to yourself.");
+            }
+
+            var sender = await _userRepo.GetUserByUsernameAsync(username);
+            var recipient = await _userRepo.GetUserByUsernameAsync(createMessageDto.RecipientUsername);
+
+            if (recipient == null)
+            {
+                return NotFound();
+            }
+
+            var message = new Message()
+            {
+                Sender = sender,
+                Recipient = recipient,
+                SenderUsername = sender.Username,
+                RecipientUsername = recipient.Username,
+                Content = createMessageDto.Content
+            };
+
+            _messageRepo.AddMessage(message);
+
+            if (await _messageRepo.SaveAllAsync())
+            {
+                return Ok(_mapper.Map<MessageDto>(message));
+            }
+
+            return BadRequest("Failed to send message.");
+        }
+    }
+}
