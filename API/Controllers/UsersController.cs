f68c3d45f0e964026eac5798438762ba1c939014
Pradip <Pradip@DESKTOP-7D2H6S8>
2025-11-18 09:35:40 +0530

    Adding Admin Component & Guard in Asp.Net Core part 133

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -26,7 +26,7 @@ namespace API.Controllers
             _photoService = photoService;
         }
 
-        [Authorize(Roles = "Admin")]
+
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers([FromQuery]UserParams userParams)
         {
@@ -45,7 +45,7 @@ namespace API.Controllers
             return Ok(users);
         }
 
-        [Authorize(Roles = "Member")]
+
         [HttpGet("{username}", Name = "GetUser")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
